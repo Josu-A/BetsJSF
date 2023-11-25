06e9254b1cb462f797bfbaa9d3cefb2be8c28dd6
Josu-A <aginagajosu@gmail.com>
2023-11-25 23:06:15 +0100

    Add question querying functionality

--- src/main/java/eredua/bean/MainBean.java
+++ src/main/java/eredua/bean/MainBean.java
@@ -1,6 +1,5 @@
 package eredua.bean;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -12,13 +11,16 @@ import org.primefaces.event.SelectEvent;
 
 import businessLogic.BLFacade;
 import domain.Event;
+import domain.Question;
 
 public class MainBean {
 	
 	private BLFacade facadeBL;
 	private Date data;
 	private Event gertaera;
+	private Question galdera;
 	private static List<Event> gertaerak;
+	private static List<Question> galderak;
 	
 	public MainBean() {
 		this.facadeBL = FacadeBean.getBusinessLogic();
@@ -33,45 +35,74 @@ public class MainBean {
 		return this.data;
 	}
 	
+	public Question getGaldera() {
+		return this.galdera;
+	}
+	
 	public Event getGertaera() {
 		return this.gertaera;
 	}
 	
+	@SuppressWarnings("static-method")
+	public List<Question> getGalderak() {
+		return galderak;
+	}
+	
+	@SuppressWarnings("static-method")
 	public List<Event> getGertaerak() {
 		return gertaerak;
 	}
 	
-	public static Event getObject(String description) {
-		for (Event e : gertaerak) {
-			if (description.equals(e.getDescription())) {
-				return e;
-			}
-		}
-		return null;
-	}
-	
 	public void onDateSelect(SelectEvent event) {
+		this.resetSelections();
 		gertaerak = this.facadeBL.getEvents(this.data);
-		System.out.println(gertaerak.get(0).getDescription());
 		FacesContext.getCurrentInstance().addMessage(null,
 				new FacesMessage("Data aukeraua: " + event.getObject()));
 	}
 	
+	public void onGalderaSelect(SelectEvent event) {
+		FacesContext.getCurrentInstance().addMessage("nireForm:mezuak",
+				new FacesMessage("Galdera (taula): " + this.galdera.getQuestionNumber()
+						+ "/" + this.galdera.getQuestion()));
+	}
+	
 	public void onGertaeraSelect(SelectEvent event) {
+		this.resetGalderaSelection();
 		this.gertaera = (Event) event.getObject();
 		FacesContext.getCurrentInstance().addMessage("nireForm:mezuak",
 				new FacesMessage("Gertaera (taula): " + this.gertaera.getEventNumber()
 						+ "/" + this.gertaera.getDescription()));
+		galderak = this.gertaera.getQuestions();
+	}
+	
+	public void resetGalderaSelection() {
+		this.galdera = null;
+		galderak = null;
+	}
+	
+	public void resetSelections() {
+		this.gertaera = null;
+		this.resetGalderaSelection();
 	}
 	
 	public void setData(Date data) {
 		this.data = data;
 	}
 	
+	public void setGaldera(Question galdera) {
+		this.galdera = galdera;
+	}
+	
 	public void setGertaera(Event gertaera) {
 		this.gertaera = gertaera;
 	}
 	
+	@SuppressWarnings("static-method")
+	public void setGalderak(List<Question> gL) {
+		galderak = gL;
+	}
+	
+	@SuppressWarnings("static-method")
 	public void setGertaerak(List<Event> gL) {
 		gertaerak = gL;
 	}
